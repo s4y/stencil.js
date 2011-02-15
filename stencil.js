6afd7e726f7409c5e0cae26445cc4bd9d1518ec8
Sidney San Martín <s@sidneysm.com>
2011-02-14 21:55:05 -0500

    Major API change, more/better functionality. stencil directives are now prefixed with $, in the style of MongoDB. Docs forthcoming.

--- stencil.js
+++ stencil.js
@@ -6,32 +6,41 @@
 				template.splice(i, 1, stencil(template[i], data, i));
 			}
 		} else if($.isPlainObject(template)) {
-			if ('join' in template) {
-				return stencil(template.join, data).join(template.separator || '');
-			} else if ('key' in template) {
-				if (template.handler) {
-					template = template.handler.call(data[template.key]);
-				} else if (template.template) {
-					return [stencil(template.template, data[template.key])];
-				} else if (template.children) {
-					template = $.map(template.key === "" ? data : data[template.key], function(item){
-						return [stencil(template.children, item)];
-					});
-				} else if (template.conditional) {
-					template = (data[template.key]) ? stencil(template.conditional, data) : [];
-				} else {
-					template = (template.key === '' ? data : template.key === '.' ? index : (data[template.key] === undefined || data[template.key] == null) ? '' : data[template.key]).toString();
+			if ('$join' in template) {
+				return stencil(template.$join, data).join(template.$separator || '');
+			} else if ('$or' in template) {
+				template = stencil(template.$or, data);
+				for(var i = 0, l = template.length; i < l; i++){
+					if (template[i]) {
+						return template[i];
+					}
 				}
+				return template[i-1];
+			} else if ('$test' in template) {
+				if (stencil(template.$test, data)) {
+					return stencil(template.$if, data) || [];
+				} else {
+					return stencil(template.$else, data) || [];
+				}
+			} else if ('$handler' in template) {
+				return template.$handler.call(template, ('$key' in template) ? data[template.$key] : data);
+			} else if ('$template' in template) {
+				return [stencil(template.$template, ('$key' in template) ? data[template.$key] : data)];
+			} else if ('$children' in template) {
+				return $.map(template.$key === "" ? data : data[template.$key], function(item){
+					return [stencil(template.$children, item)];
+				});
+			} else if ('$key' in template) {
+				template = (template.$key === '' ? data : template.$key === '.' ? index : (data[template.$key] === undefined || data[template.$key] == null) ? '' : data[template.$key]).toString();
 			} else if(index === 1) {
 				template = $.extend({}, template);
-				for (var key in template){
-					template[key] = stencil(template[key], data);
+				for (var $key in template){
+					template[$key] = stencil(template[$key], data);
 				}
 			}
 		}
 		return template;
 	}
-	
 	window.stencil = stencil;
 	jQuery.fn.stencil = function(template, data){
 		this.haml(stencil(template, data));
