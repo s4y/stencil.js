3c093e8ac89e1b20c5c1dc5a6e3529218603123f
Sidney San Martín <s@sidneysm.com>
2011-02-22 16:14:17 -0500

    Add support for iterating through objects with `$children`.
    - The name of the current key can be accessed with `{ $key: '.' }`.

--- stencil.js
+++ stencil.js
@@ -1,15 +1,15 @@
 (function(){
-	function stencil(template, data, index){
+	function stencil(template, data, index, dataIndex){
 		if ($.isArray(template)) {
 			template = Array.prototype.slice.call(template, 0);
 			for(var i = 0, l = template.length; i < l; i++){
-				template.splice(i, 1, stencil(template[i], data, i));
+				template.splice(i, 1, stencil(template[i], data, i, dataIndex));
 			}
 		} else if($.isPlainObject(template)) {
 			if ('$join' in template) {
-				return stencil(template.$join, data).join(template.$separator || '');
+				return stencil(template.$join, data, index, dataIndex).join(template.$separator || '');
 			} else if ('$or' in template) {
-				template = stencil(template.$or, data);
+				template = stencil(template.$or, data, index, dataIndex);
 				for(var i = 0, l = template.length; i < l; i++){
 					if (template[i]) {
 						return template[i];
@@ -17,25 +17,34 @@
 				}
 				return template[i-1];
 			} else if ('$test' in template) {
-				if (stencil(template.$test, data)) {
-					return stencil(template.$if, data) || [];
+				if (stencil(template.$test, data, index, dataIndex)) {
+					return stencil(template.$if, data, index, dataIndex) || [];
 				} else {
-					return stencil(template.$else, data) || [];
+					return stencil(template.$else, data, index, dataIndex) || [];
 				}
 			} else if ('$handler' in template) {
 				return template.$handler.call(template, ('$key' in template) ? data[template.$key] : data);
 			} else if ('$template' in template) {
-				return [stencil(template.$template, ('$key' in template) ? data[template.$key] : data)];
+				return [stencil(template.$template, ('$key' in template) ? data[template.$key] : data, index, ('$key' in template) ? template.$key : dataIndex)];
 			} else if ('$children' in template) {
-				return $.map(template.$key === "" ? data : data[template.$key], function(item){
-					return [stencil(template.$children, item)];
+				data = template.$key ? data[template.$key] : data;
+				if ($.isPlainObject(data)) {
+					var key, childTemplate = template.$children;
+					template = [];
+					for (key in data) {
+						template.push(stencil(childTemplate, data[key], index, key));
+					}
+					return template;
+				}
+				return $.map(data, function(item, i){
+					return [stencil(template.$children, item, i)];
 				});
 			} else if ('$key' in template) {
-				template = (template.$key === '' ? data : template.$key === '.' ? index : (data[template.$key] === undefined || data[template.$key] == null) ? '' : data[template.$key]).toString();
+				template = (template.$key === '' ? data : template.$key === '.' ? dataIndex : (data[template.$key] === undefined || data[template.$key] == null) ? '' : data[template.$key]).toString();
 			} else if(index === 1) {
 				template = $.extend({}, template);
-				for (var $key in template){
-					template[$key] = stencil(template[$key], data);
+				for (var key in template){
+					template[key] = stencil(template[key], data, key, dataIndex);
 				}
 			}
 		}
